b5535a719b615a5c2e6a18a0894a64b1c07608c9
github username <github email address>
2018-11-11 21:28:25 -0800

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -7,4 +7,19 @@ class GithubRepo
     @url = hash["html_url"]
   end
 
+  def authenticate!(client_id, client_secret, code)
+    response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.body = {"client_id": client_id, "client_secret": client_secret, "code": code}
+      req.headers['Accept'] = 'application/json'
+    end
+    access_hash = JSON.parse(response.body)
+    session[:username] = access_hash["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session_token}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+
+  end
+
+
 end
